375c41d5c39b6a671f598de34619d6ef61def647
s-ayn06 <shimizu.ayn69@outlook.jp>
2024-08-19 21:56:50 +0900

    kadai_007
    kadai_007

---
+++ kadai_007/array.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+     <p>
+     <?php
+         $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+ 
+         print_r($onion_data);
+ 
+    
+         echo '<br>';
+ 
+         
+      
+         print_r($onion_data);
+         ?>
+
+     </p>
+ </body>
+ 
+ </html>
