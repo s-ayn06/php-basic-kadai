a3355b4ae9e3352aa478a7bb8b93f57fdf6d4593
s-ayn06 <shimizu.ayn69@outlook.jp>
2024-09-02 20:32:41 +0900

    kadai_016
    kadai_016

--- kadai_016/class.php
+++ kadai_016/class.php
@@ -10,17 +10,24 @@
      <p>
      <?php
 class Food {
+    private$name;
+    private$price;
+
     public function __construct(string $name, int $price) {
         $this->name = $name;
         $this->price = $price;
     }
     
     public function show_price() {
-        return $this->price;
+        echo $this->price;
     }
 }
 
 class Animal {
+    private$name;
+    private$height;
+    private$weight;
+
     public function __construct(string $name, int $height, int $weight) {
         $this->name = $name;
         $this->height = $height;
@@ -28,9 +35,21 @@ class Animal {
     }
     
     public function show_height() {
-        return $this->height;
+        echo $this->height;
     }
 }
+    $potato=new food('potato',250);
+    $dog=new animal('dog',60,5000);
+
+    print_r($potato);
+    print_r($dog);
+
+    echo'<br>';
+    $potato->show_price();
+    echo'<br>';
+    $dog->show_height();
+
+
 
          ?>
      </p>
